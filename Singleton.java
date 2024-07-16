a20c320448081514403024f78131f549544cc011
Zeyad Wael <127014710+FearLeSS-21@users.noreply.github.com>
2024-07-16 12:23:56 +0300

    Add files via upload

---
+++ Singleton.java
@@ -0,0 +1,19 @@
+public class Singleton {
+    private static Singleton InstanceValue;
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstanceofvalue() {
+        if (InstanceValue == null) {
+            InstanceValue= new Singleton();
+        }
+        return InstanceValue;
+
+    }
+    public void PrintData(){
+
+        System.out.println("Ameirka yahhh");
+    }
+
+}
